42e087e99395af1445726b9ce15c11603cfdf8dc
Tane4ka170 <tane4ka170@gmail.com>
2024-10-16 19:58:14 +0300

    Added weight tracking functionality with input and display of current weight

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
-<script setup></script>
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+// import Chart from "chart.js/auto";
+
+const weights = ref([]);
+
+const weightChartEl = ref(null);
+
+const weightChart = shallowRef(null);
+
+const weightInput = ref(0);
+
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 };
+});
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput.value,
+    date: new Date().getTime(),
+  });
+};
+</script>
 
 <template>
-  <h1>predacean</h1>
+  <main>
+    <h1>WeightWatch</h1>
+
+    <div class="current">
+      <span>{{ currentWeight.weight }}</span>
+      <small>Current Weight (kg)</small>
+    </div>
+
+    <form @submit.prevent="" addWeight>
+      <input type="number" step="0.1" v-model="weightInput" />
+    </form>
+  </main>
 </template>
 
 <style scoped></style>
